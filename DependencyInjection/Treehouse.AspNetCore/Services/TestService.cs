972accbb27f84084059beff84eca2c1c4c37875e
Ariel R <rzeszowsky@gmail.com>
2020-03-09 23:02:51 +0100

    Add test class and register as a Service

---
+++ DependencyInjection/Treehouse.AspNetCore/Services/TestService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Treehouse.AspNetCore.Services
+{
+    public class TestService
+    {
+
+        public string GetAboutContent()
+        {
+            return "Here's the About view content from the TestService";
+        }
+    }
+}
