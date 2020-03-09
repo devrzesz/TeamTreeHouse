2e4d0d605e115fb940c7f676014e35c51fa14729
Ariel R <rzeszowsky@gmail.com>
2020-03-09 22:34:53 +0100

    Add SimpleInjector and configure

---
+++ DependencyInjection/Treehouse.SolarSystem/App_Start/SimpleInjectorInitializer.cs
@@ -0,0 +1,35 @@
+[assembly: WebActivator.PostApplicationStartMethod(typeof(Treehouse.SolarSystem.App_Start.SimpleInjectorInitializer), "Initialize")]
+
+namespace Treehouse.SolarSystem.App_Start
+{
+    using System.Reflection;
+    using System.Web.Mvc;
+
+    using SimpleInjector;
+    using SimpleInjector.Integration.Web;
+    using SimpleInjector.Integration.Web.Mvc;
+    using Treehouse.SolarSystem.Data;
+
+    public static class SimpleInjectorInitializer
+    {
+        public static void Initialize()
+        {
+            var container = new Container();
+            container.Options.DefaultScopedLifestyle = new WebRequestLifestyle();
+            
+            InitializeContainer(container);
+
+            container.RegisterMvcControllers(Assembly.GetExecutingAssembly());
+            
+            container.Verify();
+            
+            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
+        }
+     
+        private static void InitializeContainer(Container container)
+        {
+            container.Register<Context>(Lifestyle.Scoped);
+            container.Register<Repository>(Lifestyle.Scoped);
+        }
+    }
+}
