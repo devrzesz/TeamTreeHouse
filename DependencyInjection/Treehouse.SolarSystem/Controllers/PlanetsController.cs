ba0d3fb17e5b3efe3af07ab9b3cb307d6375a58a
Ariel R <rzeszowsky@gmail.com>
2020-03-09 20:10:19 +0100

    Initial commit
    Initial commit with downloaded files from tth. Simple ASP.NET project

---
+++ DependencyInjection/Treehouse.SolarSystem/Controllers/PlanetsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Treehouse.SolarSystem.Data;
+
+namespace Treehouse.SolarSystem.Controllers
+{
+    public class PlanetsController : Controller
+    {
+        private bool _disposed = false;
+        private Context _context = null;
+        private Repository _repository = null;
+
+        public PlanetsController()
+        {
+            _context = new Context();
+            _repository = new Repository(_context);
+        }
+
+        public ActionResult Index()
+        {
+            var planets = _repository.GetPlanets();
+
+            return View(planets);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+
+            _disposed = true;
+
+            base.Dispose(disposing);
+        }
+    }
+}
